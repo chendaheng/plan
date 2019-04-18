9fd72993a865aa9baaebc215dc7b0f4597fac5c3
dddddh <497367544@qq.com>
2019-04-18 13:16:45 +0800

    20190418完善系列\款式组\款式的数据库

--- planservice.sql
+++ planservice.sql
@@ -72,7 +72,7 @@ DROP TABLE IF EXISTS `range`;
 CREATE TABLE `range` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `number` varchar(20) NOT NULL,
-  `name` varchar(20) NOT NULL,
+  `name` varchar(50) NOT NULL,
   `customerId` int(11) NOT NULL,
   `brandId` int(11) NOT NULL,
   `clothingLevelId` int(11) NOT NULL,
@@ -121,7 +121,7 @@ CREATE TABLE `style` (
   `styleGroupNumber` varchar(20) DEFAULT '',
   `styleGroupName` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT '',
   `addingMode` tinyint(4) NOT NULL,
-  `state` tinyint(4) NOT NULL DEFAULT '1' COMMENT '1为未绑定，2未绑定',
+  `state` tinyint(4) NOT NULL DEFAULT '1' COMMENT '1为未绑定，2为绑定',
   `createTime` datetime NOT NULL,
   `havePlan` bit(1) NOT NULL DEFAULT b'0' COMMENT '0为false，1为true',
   PRIMARY KEY (`id`),
@@ -157,4 +157,32 @@ INSERT INTO `clothinglevel` VALUES(3,"品牌","品牌层次");
 INSERT INTO `product`  VALUES(1,"p01","面料","面料产品",1);
 INSERT INTO `product`  VALUES(2,"p02","工艺单","工艺单产品",2);
 INSERT INTO `product`  VALUES(3,"p03","样板","样板产品",3);
+
+-- ----------------------------
+-- -- Records of range
+-- -- ----------------------------
+INSERT INTO  `range` VALUES(1,"XL20190101001","Fall-2019(07/08/09)",1,1,1,10,1,1,"2018-07-20 10:00:00","系列备注1",0,0);
+INSERT INTO  `range` VALUES(2,"XL20190101002","Spring-2019(01/02/03)",1,2,2,5,1,1,"2018-07-22 10:00:00","系列备注2",0,0);
+INSERT INTO  `range` VALUES(3,"XL20190501003","Winter-2019(10/11/12)",2,3,3,5,1,1,"2018-07-24 10:00:00","系列备注3",0,0);
+INSERT INTO  `range` VALUES(4,"XL20190501004","Summer-2019(08/09/10)",3,4,1,2,1,1,"2018-07-26 10:00:01","系列备注4",0,0);
+
+-- ----------------------------
+-- -- Records of stylegroup
+-- -- ----------------------------
+INSERT INTO  `stylegroup` VALUES(1,"KSZ20190101001","款式1组",1,1,"2018-07-20 12:00:00",0);
+INSERT INTO  `stylegroup` VALUES(2,"KSZ20190101002","款式2组",1,1,"2018-07-22 12:00:00",0);
+INSERT INTO  `stylegroup` VALUES(3,"KSZ20190101003","款式3组",2,1,"2018-07-24 12:00:01",0);
+INSERT INTO  `stylegroup` VALUES(4,"KSZ20190101004","款式4组",3,1,"2018-07-26 12:00:01",0);
+
+-- ----------------------------
+-- -- Records of style
+-- -- ----------------------------
+INSERT INTO  `style` VALUES(1,"10190114(CX1901)",1,1,"KSZ20190101001","款式1组",1,1,"2018-08-20 12:00:00",0);
+INSERT INTO  `style` VALUES(2,"10190114(CX1902)",1,1,"KSZ20190101001","款式1组",1,1,"2018-08-21 12:00:00",0);
+INSERT INTO  `style` VALUES(3,"10190114(CX1903)",1,1,"KSZ20190101001","款式1组",1,1,"2018-08-23 12:00:00",0);
+INSERT INTO  `style` VALUES(4,"10190114(CX1904)",1,2,"KSZ20190101002","款式2组",1,1,"2018-08-24 12:00:00",0);
+INSERT INTO  `style` VALUES(5,"10190114(CX1905)",1,2,"KSZ20190101002","款式2组",1,1,"2018-08-25 12:00:00",0);
+INSERT INTO  `style` VALUES(6,"10190114(CX1906)",2,3,"KSZ20190101003","款式3组",1,1,"2018-08-26 12:00:00",0);
+INSERT INTO  `style` VALUES(7,"10190114(CX1907)",2,3,"KSZ20190101003","款式3组",1,1,"2018-08-27 12:00:00",0);
+INSERT INTO  `style` VALUES(8,"10190114(CX1908)",3,4,"KSZ20190101004","款式4组",1,1,"2018-08-28 12:00:00",0);
 
